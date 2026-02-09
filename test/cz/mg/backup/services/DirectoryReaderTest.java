8128006ce2bb914418b08b52ccad7c830019d42d
mg <gekoncze@seznam.cz>
2026-02-09 20:05:05 +0100

    added more assertions for DirectoryReaderTest

--- test/cz/mg/backup/services/DirectoryReaderTest.java
+++ test/cz/mg/backup/services/DirectoryReaderTest.java
@@ -36,27 +36,41 @@ public @Test class DirectoryReaderTest {
         Assert.assertEquals(NAME, directory.getRelativePath());
         Assert.assertEquals(1, directory.getFiles().count());
         Assert.assertEquals(2, directory.getDirectories().count());
+        Assert.assertEquals(16L, directory.getProperties().getTotalSize());
+        Assert.assertEquals(4L, directory.getProperties().getTotalCount());
+        Assert.assertEquals(2L, directory.getProperties().getTotalFileCount());
+        Assert.assertEquals(2L, directory.getProperties().getTotalDirectoryCount());
 
         File file = directory.getFiles().get(0);
         Assert.assertEquals(PATH.resolve("root.txt"), file.getPath());
         Assert.assertEquals(NAME.resolve("root.txt"), file.getRelativePath());
+        Assert.assertEquals(3L, file.getProperties().getSize());
 
         Directory one = directory.getDirectories().get(0);
         Assert.assertNull(one.getError());
         Assert.assertEquals(PATH.resolve("one"), one.getPath());
         Assert.assertEquals(NAME.resolve("one"), one.getRelativePath());
         Assert.assertEquals(1, one.getFiles().count());
+        Assert.assertEquals(13L, one.getProperties().getTotalSize());
+        Assert.assertEquals(1L, one.getProperties().getTotalCount());
+        Assert.assertEquals(1L, one.getProperties().getTotalFileCount());
+        Assert.assertEquals(0L, one.getProperties().getTotalDirectoryCount());
 
         File innerFile = one.getFiles().get(0);
         Assert.assertNull(innerFile.getError());
         Assert.assertEquals(PATH.resolve("one").resolve("file"), innerFile.getPath());
         Assert.assertEquals(NAME.resolve("one").resolve("file"), innerFile.getRelativePath());
+        Assert.assertEquals(13L, innerFile.getProperties().getSize());
 
         Directory two = directory.getDirectories().get(1);
         Assert.assertNull(two.getError());
         Assert.assertEquals(PATH.resolve("two"), two.getPath());
         Assert.assertEquals(NAME.resolve("two"), two.getRelativePath());
         Assert.assertEquals(0, two.getFiles().count());
+        Assert.assertEquals(0L, two.getProperties().getTotalSize());
+        Assert.assertEquals(0L, two.getProperties().getTotalCount());
+        Assert.assertEquals(0L, two.getProperties().getTotalFileCount());
+        Assert.assertEquals(0L, two.getProperties().getTotalDirectoryCount());
 
         progress.verify(0L, 6L);
     }
